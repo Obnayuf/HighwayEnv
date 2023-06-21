98813a4afbf5ac6865b60e91b1943c36e4c36476
Obnayuf <87928464+Obnayuf@users.noreply.github.com>
2023-06-21 18:32:07 +0800

    Update objects.py
    change the length to 5, a standard plot should be same as our veichle

--- highway_env/vehicle/objects.py
+++ highway_env/vehicle/objects.py
@@ -19,7 +19,7 @@ class RoadObject(ABC):
     For now we assume all objects are rectangular.
     """
 
-    LENGTH: float = 2  # Object length [m]
+    LENGTH: float = 5  # Object length [m]
     WIDTH: float = 2  # Object width [m]
 
     def __init__(self, road: 'Road', position: Sequence[float], heading: float = 0, speed: float = 0):
